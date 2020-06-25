e04ca246623e322d29c2a9a8ed9eeb5ad845f774
SWAPNIL MANU <swapnilsaxena149@gmail.com>
2020-06-25 11:06:09 +0530

    JS file

---
+++ myscripts.js
@@ -0,0 +1,12 @@
+function getpass(){
+			var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*(){}:<>,/.][";
+			var passwordLength = 8;
+			var password="";
+
+			for(var i=0; i<passwordLength; i++)
+			{
+				var randomNumber = Math.floor(Math.random() * chars.length);
+				password += chars.substring(randomNumber,randomNumber+1);
+			}
+			document.getElementById("password").value = password;
+		}
